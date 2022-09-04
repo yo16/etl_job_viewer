101c9f5e7e463d460102e80bcc7c7549ca2bace2
yo16 <xxyo1xx@gmail.com>
2022-09-05 00:25:13 +0900

    概要はできた

--- example/sample_main.js
+++ example/sample_main.js
@@ -1,72 +1,185 @@
 
+// ETLデータ定義
+var etl_defs = {
+	"jobs": [
+		{
+			"job_name": "job01",
+			"out_tables": ["tbl01", "tbl02", "tbl03"]
+		},
+		{
+			"job_name": "job11",
+			"in_tables": ["tbl01", "tbl02"],
+			"out_tables": ["tbl11", "tbl12"]
+		},
+		{
+			"job_name": "job21",
+			"in_tables": ["tbl03", "tbl11"],
+			"out_tables": ["tbl21"]
+		},
+		{
+			"job_name": "job31",
+			"in_tables": ["tbl01", "tbl11", "tbl21"],
+			"out_tables": ["tbl31"]
+		}
+	]
+};
 
+// ETLデータの解釈
+// 作成するオブジェクト（配列）
+// 1:nodes
+//      name        : ジョブ名 or テーブル名
+//      level       : 一番最初を0とするレベル
+//      type        : 0:ジョブ, 1:テーブル
+// 2:links
+//      source      : 元node
+//      target      : 先node
+function parse_etl_defs(df){
+    // get_level
+    // levelを返す
+    // 検査済みのnodeを取っておけば効率化できるけどめんどくさいから後回し
+    function get_level(df_, name, job_or_table/*0:job, 1:table*/){
+        let ret_val = 0;
+        let job_def = df_["jobs"];
+        if(job_or_table==0){
+            // job
+            let cur_job = job_def.find(e => e["job_name"]==name);
+            if( "in_tables" in cur_job ){
+                let max_level = 0;
+                for( t of cur_job["in_tables"] ){
+                    let cur_t_level = get_level(df_, t, 1);
+                    if( cur_t_level > max_level ){
+                        max_level = cur_t_level;
+                    }
+                }
+                ret_val = max_level + 1;
+            }else{
+                ret_val = 0;
+            }
+        }else{
+            // table
+            // 前提: 特定のテーブルを作成するジョブは一意である
+            let cur_job = job_def.find(e => (e["out_tables"].indexOf(name)>=0));
+            ret_val = get_level(df_, cur_job["job_name"], 0)+1;
+        }
+        return ret_val;
+    }
+    var nodes = [];
+    var links = [];
 
-
-//ノードのデータを作成
-var range = 10;
-var nodes = d3.range(0, range).map(
-    function(d,i){
-        return {
-            id:i,
-            label: "l"+d ,
-            //r:~~d3.randomUniform(5, 60)()
-            r:30
+    for( j of df["jobs"] ){
+        // job
+        nodes.push({
+            "name"  : j["job_name"],
+            "level" : get_level(df, j["job_name"], 0),
+            "type"  : 0
+        });
+        if("in_tables" in j){
+            for( t of j["in_tables"] ){
+                links.push({
+                    "source"    : t,
+                    "target"    : j["job_name"]
+                });
+            }
+        }
+        for( t of j["out_tables"] ){
+            links.push({
+                "source"    : j["job_name"],
+                "target"    : t
+            });
+        }
+        // table
+        for( t of j["out_tables"] ){
+            nodes.push({
+                "name"  : t,
+                "level" : get_level(df, t, 1),
+                "type"  : 1
+            });
         }
     }
-);
 
-//メイン！D3.jsのforcesimulation
-//svg領域の大きさを定義する
-var svgheight = 960,
-      svgwidth = 1000;
-var simulation = d3.forceSimulation()
-            .force("collide",d3.forceCollide().radius(function(d){return d.r+5;}).iterations(16) ) //衝突値の設定
-            .force("charge", d3.forceManyBody().strength(-30))  //反発力の設定
-            .force("center", d3.forceCenter(svgwidth / 2, svgheight / 2))  //svg領域の中心を重力の中心とする設定
-            .force("x", d3.forceX().x(svgwidth / 2).strength(0.2))  //x方向に戻る力
-            .force("y", d3.forceY().y(svgheight / 2).strength(0.2)) //y方向に戻る力
-     ;
-     
-//ノードの色を定義
-var color = d3.scaleOrdinal(["#66c2a5", "#fc8d62", "#8da0cb", "#e78ac3", "#a6d854", "#ffd92f", "#e5c494", "#b3b3b3"]);
-//ノードをsvg領域に描画する
-var node = d3.select("svg").append("g")  //gはSVGの描画に必要なオブジェクトのコンテナ。aとかkとかはだめ。
-           .attr("class", "nodes") //gはどんな種類のオブジェクトを持つコンテナかをわかりやすく
-           .selectAll("circle") //nodeは円、つまりcircle要素で描画する(ここでは定義されず、ただ"指定(selectAll)"されているだけにすぎない
-           .data(nodes) //上部で定義した100個のnodeを持つ配列
-           .enter() //上のdataの内容を確定させる
-           .append("circle") //ここでcircleを追加し、SVG領域に描画する
-           .attr("class","node") //circle１つ１つがnodeというクラスであることを命名する
-           .attr("r", function(d){  return d.r ;}) //nodeの大きさ（半径）を指定する
-           .attr("fill",function(d){ return color(d.id);}) //nodeの色を指定する。colorは上部で定義されている
-           .call(d3.drag()  //nodeをマウスでドラッグした時に通る関数を定義。関数の内容は後ほど
-               .on("start", dragstarted)
-               .on("drag", dragged)
-               .on("end", dragended)
-           ); 
-//ノードをマウスドラッグで動かすための関数
-function dragstarted(d) {  //ドラッグ開始時の関数
-      if (!d3.event.active) simulation.alphaTarget(.03).restart();
-      d.fx = d.x;
-      d.fy = d.y;
+    return [nodes, links];
 }
-function dragged(d) {  //ドラッグ中の関数
-    d.fx = d3.event.x;
+var [job_nodes, job_links] = parse_etl_defs(etl_defs);
+console.log({job_nodes});
+//console.log({job_links});
+
+
+// 描画するところ
+var svg_g = d3.select("svg").append("g");
+//svg領域の大きさを定義する
+var svgheight = 960, svgwidth = 1000;
+
+// link
+var links = svg_g.selectAll(".link")
+        .data(job_links)
+        .enter()
+        .append("line")
+        .attr("class", "link")
+        .attr("stroke", "#666")
+        .attr("stroke-weight", 1)
+;
+
+// node
+var nodes = svg_g.selectAll("circle")
+        .data(job_nodes)
+        .enter()
+        .append("circle")
+        .attr("class", "job-circle")
+        .attr("r", function(d){ return (d["type"]==0)? 20:10; })
+        .attr("fill", function(d){ return (d["type"]==0)? "#f00":"#00f"; })
+        .call(d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended)
+        )
+        .on("click", function(d){ console.log(d.name); })
+;
+
+var line_force = d3.forceSimulation()
+        .nodes(job_nodes)
+        .on("tick", ticked)
+        .force(
+            "link",
+            d3.forceLink(job_links)
+                .id(function(d){ return d.name; })
+                .distance(20)
+        )
+        .force("center", d3.forceY(svgheight/2))
+        .force("charge", d3.forceManyBody().strength(-20))
+        .force("collide", d3.forceCollide(40))
+        .velocityDecay(0.5)
+;
+for( let i=0; i<job_nodes.length; i++ ){
+    line_force.nodes()[i].fx = 50 + job_nodes[i].level * 100;
+}
+
+//ドラッグ開始時
+function dragstarted(d) {
+    if (!d3.event.active) line_force.alphaTarget(0.9).restart();
+    //d.fx = d.x;
+    d.fy = d.y;
+}
+// ドラッグ中
+function dragged(d) {
+    //d.fx = d3.event.x;
     d.fy = d3.event.y;
 }
-function dragended(d) { //ドラッグ終了時の関数
-    if (!d3.event.active) simulation.alphaTarget(.03);
-    d.fx = null;
+//ドラッグ終了時
+function dragended(d) {
+    if (!d3.event.active) line_force.alphaTarget(.03);
+    //d.fx = null;
     d.fy = null;
 }
-
-//動作開始！
-simulation
-    .nodes(nodes)
-    .on("tick", ticked);
-
-function ticked() {
-    node
-        .attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
-} 
+// tick
+function ticked(){
+    nodes
+        .attr("cx", function(d){ return d.x; })
+        .attr("cy", function(d){ return d.y; })
+    ;
+    links
+        .attr("x1", function(d){ return d.source.x; })
+        .attr("y1", function(d){ return d.source.y; })
+        .attr("x2", function(d){ return d.target.x; })
+        .attr("y2", function(d){ return d.target.y; })
+    ;
+}
